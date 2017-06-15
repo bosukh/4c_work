2790b2778d80963e56894dc5c3bd41900a34e5ff
BoSuk Ben Hong <benhong@BoSuks-MacBook-Pro.local>
2017-06-15 12:21:04 -0500

    no need to set transaction isolation level

--- vizio_data_import_script.sh
+++ vizio_data_import_script.sh
@@ -14,7 +14,6 @@ SET foreign_key_checks=0;
 SET unique_checks=0;
 SET sql_log_bin=0;
 SET autocommit=0;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 LOAD DATA LOCAL INFILE '$csv_filename' INTO TABLE $db_name
 FIELDS TERMINATED BY '^'
 LINES TERMINATED BY '\n';
@@ -23,5 +22,4 @@ SET sql_log_bin=1;
 SET unique_checks=1;
 SET foreign_key_checks=1;
 SET autocommit=1;
-SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 QUERY_INPUT
