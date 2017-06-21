b14cd39ccd2f4b252f26a39295ac12f636eb04fc
BoSuk Ben Hong <benhong@BoSuks-MacBook-Pro.local>
2017-06-21 12:15:01 -0500

    check before importing file

--- vizio_main.py
+++ vizio_main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from datetime import datetime
+from datetime import datetime, date
 from local_logger import LocalLogger
 from vizio_data_import import VizioImporter
 from vizio_file_download import VizioFileDownloader
@@ -13,6 +13,8 @@ logger = LocalLogger(
         ).logger
 
 def main(year, month, day, file_path):
+    date_str = date(year, month, day).strftime('%Y-%m-%d')
+    logger.info('Running the script for %s'%date_str)
     importer = VizioImporter(year, month, day)
     downloader = VizioFileDownloader(importer, year, month, day)
     folder_path = downloader.download(path = file_path)
@@ -22,15 +24,24 @@ def main(year, month, day, file_path):
             files.append(file_name)
     files.sort()
     for file_name in files:
-        importer.import_file(os.path.join(folder_path, file_name))
+        current_fileinfo = importer.fileinfo.loc[importer.fileinfo.file_name == file_name]
+        if len(current_fileinfo) == 0:
+            logger.warning('Table and local directory out of sync. Check %s'%file_name)
+            continue
+        if current_fileinfo['imported_date'].isnull().sum() > 0:
+            importer.import_file(os.path.join(folder_path, file_name))
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print 'date string "YYYY-MM-DD" is needed'
+    args = {}
+    for arg in sys.argv[1:]:
+        k, v = arg.split('=', 1)
+        args[k.strip()] = v.strip()
+    date_str = args.get('date')
+    file_path = args.get('file_path')
+    if date_str is None:
+        print "Date is not specified, running for the scrip for today's date"
+        today = date.today()
+        year, month, day = today.year, today.month, today.day
     else:
-        date_str = sys.argv[1]
-        file_path = None
-        if len(sys.argv) > 2:
-            file_path = sys.argv[2]
         year, month, day = [int(x) for x in date_str.split('-')]
-        main(year, month, day, file_path)
+    main(year, month, day, file_path)
